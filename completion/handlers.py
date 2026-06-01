8a9ddeedb590a4afc4737aa3159bad79129b8310
andrey-canon <andrey.canon@edunext.co>
2026-06-01 18:10:13 -0500

    feat(completion): add async processing capability for block completion
    Introduces the capability to process block completions asynchronously via Celery
    to mitigate severe database lock contention on the `completion_blockcompletion`
    table during high-concurrency events.
    
    - Created `submit_block_completion_async` Celery task to handle DB inserts.
    - Updated `scorable_block_completion` handler to route tasks using `.apply_async()` or `.apply()`.
    - Introduced `completion.enable_async_completion` waffle switch to safely toggle this feature.

--- completion/handlers.py
+++ completion/handlers.py
@@ -5,13 +5,9 @@ Signal handlers to trigger completion updates.
 import logging
 
 from django.contrib import auth
-from opaque_keys import InvalidKeyError
-from opaque_keys.edx.keys import LearningContextKey, UsageKey
-from xblock.completable import XBlockCompletionMode
-from xblock.core import XBlock
 
 from . import waffle
-from .models import BlockCompletion
+from .tasks import submit_block_completion_async
 
 log = logging.getLogger(__name__)
 User = auth.get_user_model()
@@ -23,31 +19,26 @@ def scorable_block_completion(sender, **kwargs):  # pylint: disable=unused-argum
     """
     if not waffle.ENABLE_COMPLETION_TRACKING_SWITCH.is_enabled():
         return
-    try:
-        block_key = UsageKey.from_string(kwargs['usage_id'])
-    except InvalidKeyError:
-        log.exception("Unable to parse XBlock usage_id for completion: %s", kwargs['usage_id'])
+
+    usage_id = kwargs.get('usage_id')
+    user_id = kwargs.get('user_id')
+    course_id = kwargs.get('course_id')
+    score_deleted = kwargs.get('score_deleted', False)
+    grader_response = kwargs.get('grader_response', False)
+
+    if not all([usage_id, user_id, course_id]):
+        log.warning("Missing required kwargs for scorable_block_completion.")
         return
 
-    if block_key.context_key.is_course and block_key.context_key.run is None:
-        # In the case of old mongo courses, the context_key cannot be derived
-        # from the block key alone since it will be missing run info:
-        course_key_with_run = LearningContextKey.from_string(kwargs['course_id'])
-        block_key = block_key.replace(course_key=course_key_with_run)
+    task_kwargs = {
+        'user_id': user_id,
+        'usage_id': usage_id,
+        'course_id': course_id,
+        'score_deleted': score_deleted,
+        'grader_response': grader_response
+    }
 
-    block_cls = XBlock.load_class(block_key.block_type)
-    if XBlockCompletionMode.get_mode(block_cls) != XBlockCompletionMode.COMPLETABLE:
-        return
-    if getattr(block_cls, 'has_custom_completion', False):
-        return
-    user = User.objects.get(id=kwargs['user_id'])
-    if kwargs.get('score_deleted'):
-        completion = 0.0
+    if waffle.ENABLE_ASYNC_COMPLETION_SWITCH.is_enabled():
+        submit_block_completion_async.apply_async(kwargs=task_kwargs)
     else:
-        completion = 1.0
-    if not kwargs.get('grader_response'):
-        BlockCompletion.objects.submit_completion(
-            user=user,
-            block_key=block_key,
-            completion=completion,
-        )
+        submit_block_completion_async.apply(kwargs=task_kwargs)
